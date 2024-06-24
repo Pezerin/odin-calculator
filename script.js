9b865a80687644a5938cdeeb1940d07cf5cf18fe
Mikul Shah <mikulshah2008@gmail.com>
2024-06-24 15:26:03 -0400

    Added all buttons and operations to the js file and added functionality for the AC, plus/minus, and percent buttons

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ const operate = (first, operator, second) => {
         return divide(first, second);
     }
 }
+
+let displayValue = 0;
+const display = document.querySelector(".numbers")
+
+const ac = document.querySelector(".ac");
+const plusminus = document.querySelector(".plusminus");
+const percent = document.querySelector(".percent");
+const divison = document.querySelector(".divison");
+
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const multiplication = document.querySelector(".multiplication");
+
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const subtraction = document.querySelector(".subtraction");
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const addition = document.querySelector(".addition");
+
+const zero = document.querySelector(".zero");
+const decimal = document.querySelector(".decimal");
+const equals = document.querySelector(".equals");
+
+ac.addEventListener("click", () => {
+    displayValue = 0;
+    display.textContent = displayValue;
+});
+
+plusminus.addEventListener("click", () => {
+    displayValue *= -1;
+    display.textContent = displayValue;
+});
+
+percent.addEventListener("click", () => {
+    if (displayValue !== 0) {
+        displayValue /= 100;
+    }
+    display.textContent = displayValue.toString();
+});
